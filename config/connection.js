2ac99d7c83836623785eeba6aeb94d008d55c809
Gautam Bhesaniya <gautam.nexios@gmail.com>
2024-04-18 10:59:50 +0530

    m

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://sahilnexios:PvCNYoO1aT3q8Qx2@cluster0.zsraiar.mongodb.net/test").then(() => {
+mongoose.connect("mongodb+srv://sahilnexios:PvCNYoO1aT3q8Qx2@cluster0.zsraiar.mongodb.net/test", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
     console.log("connection Succesfully")
 }).catch((err) => {
     console.log("connection faield", err)
